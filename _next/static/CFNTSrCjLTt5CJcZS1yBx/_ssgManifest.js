86312980e7d9554aeed0d084b54ba36722465e0f
biggleem <biggleem@gmail.com>
2026-04-06 03:07:40 -0400

    Fix crowdfund route + tested pledge flow

---
+++ _next/static/CFNTSrCjLTt5CJcZS1yBx/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fbusinesses\u002F[orgId]","\u002Fcrowdfund\u002Fcampaign\u002F[slug]","\u002Fcrowdfund\u002Fcampaign\u002F[slug]\u002Fpledge","\u002Fhr\u002F[staffId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
